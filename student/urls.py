4f05bc3bf551c8be15c28eab9a8939bb8b42939b
Usman <usman.azam96@gmail.com>
2020-05-08 00:00:30 +0500

    first commit

---
+++ student/urls.py
@@ -0,0 +1,25 @@
+"""RTS URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from typing import List, Any
+
+from django.contrib import admin
+from django.urls import path
+from student import views
+
+urlpatterns = [
+    path("", views.home, name='student home'),
+
+]
